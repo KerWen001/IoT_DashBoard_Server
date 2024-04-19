2e641f4d96a8d1091d4a548d84f522d754ccf91b
KerWen001 <dominik.gumola@gmail.com>
2024-04-19 11:10:10 +0200

    lab7

--- lib/controllers/data.controller.ts
+++ lib/controllers/data.controller.ts
@@ -1,11 +1,14 @@
 import Controller from '../interfaces/controller.interface';
 import {Request, Response, NextFunction, Router, text} from 'express';
+import {checkIdParam} from "../middlewares/deviceIdParam.middleware";
+import DataService from "../modules/services/data.service";
 
 let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
 
 class DataController implements Controller {
     public path = '/api/data';
     public router = Router();
+    public dataService = new DataService();
 
     constructor() {
         this.initializeRoutes();
@@ -13,11 +16,16 @@ class DataController implements Controller {
 
     private initializeRoutes() {
         this.router.get(`${this.path}/latest`, this.getLatestReadingsFromAllDevices);
-        this.router.post(`${this.path}/post/:id`, this.addData);
-        this.router.get( `${this.path}/get/:id`, this.getDataByValue);
-        this.router.get( `${this.path}/get/:id/:num`, this.getDataAmountByValue);
-        this.router.delete( `${this.path}/delete/:id`, this.deleteDataByValue);
-        this.router.delete( `${this.path}/delete/all`, this.deleteAllDataByValue);
+        this.router.get(`${this.path}/:id/data`,checkIdParam, this.getAllDeviceData);
+        this.router.get(`${this.path}/:id/latest`,checkIdParam, this.getLatestDeviceData);
+        // this.router.get(`${this.path}/latestAll`, this.getLatestDeviceDataFromAllDevices);
+        this.router.post(`${this.path}/:id/data`,checkIdParam, this.addDataToDataBase);
+        this.router.post(`${this.path}/:id`,checkIdParam, this.addData);
+        this.router.get( `${this.path}/:id`,checkIdParam, this.getDataByValue);
+        this.router.get( `${this.path}/:id/:num`,checkIdParam, this.getDataAmountByValue);
+        this.router.delete( `${this.path}/:id`,checkIdParam, this.deleteDataByValue);
+        this.router.delete( `${this.path}/:id/latest`,checkIdParam, this.deleteDataFromDevice);
+        this.router.delete( `${this.path}/all`, this.deleteAllDataByValue);
 
     }
 
@@ -50,6 +58,7 @@ class DataController implements Controller {
         response.status(200).json(testArr);
     };
 
+
     private deleteAllDataByValue = async (request: Request, response: Response, next: NextFunction) => {
         testArr = [];
 
@@ -58,11 +67,59 @@ class DataController implements Controller {
 
     private addData = async (request: Request, response: Response, next: NextFunction) => {
     const { elem } = request.body;
+    //const { id } = request.params;
 
 testArr.push(elem);
 
 response.status(200).json(testArr);
 };
+
+    private getAllDeviceData = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+        const allData = await this.dataService.query(id);
+        response.status(200).json(allData);
+    };
+
+    private getLatestDeviceData = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+        const Data = await this.dataService.lastestData(id);
+        response.status(200).json(Data);
+    };
+
+    // private getLatestDeviceDataFromAllDevices = async (request: Request, response: Response, next: NextFunction) => {
+    //     const Data = await this.dataService.latestDataOfAllDevices();
+    //     response.status(200).json(Data);
+    // };
+
+    private addDataToDataBase = async (request: Request, response: Response, next: NextFunction) => {
+        const { air } = request.body;
+        const { id } = request.params;
+
+        const data = {
+            temperature: air[0].value,
+            pressure: air[1].value,
+            humidity: air[2].value,
+            deviceId: id,
+            readingDate : new Date()
+        }
+
+        try {
+
+            await this.dataService.createData(data);
+            response.status(200).json(data);
+        } catch (error) {
+            console.error(`Validation Error: ${error.message}`);
+            response.status(400).json({ error: 'Invalid input data.' });
+        }
+    };
+
+    private deleteDataFromDevice = async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params;
+       const data = await this.dataService.deleteData(id);
+
+        response.status(200).json(data);
+    };
+
 }
 
 export default DataController;
